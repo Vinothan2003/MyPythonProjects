06fdf7a85ed5604973e9cc77dd13104111678e79
Vinothan2003 <vinothannc4@gmail.com>
2024-04-16 16:34:16 +0530

    day 2 : gym mini

--- com/mosh/woking_with_files/work_out_programs/gym/members_data/member_details.py
+++ com/mosh/woking_with_files/work_out_programs/gym/members_data/member_details.py
@@ -1,17 +1,22 @@
 import json
-from pathlib import Path
 
 
 class GymMembers:
-    def new_member_data(self, name, age, gender, contact_no, aadhar_no, payment_selection, amount):
-        member_details = {'name': name, 'age': age, 'gender': gender, 'contact_no': contact_no, 'aadhar_no': aadhar_no,
-                          'payment_selection': payment_selection, 'amount': amount}
+    @staticmethod
+    def new_member_data(name, age, gender, contact_no, aadhar_no, package, amount):
+        with open("gym/members_data/member_details.json", 'r') as read_file:  # reding the json file
+            existing_data = json.load(read_file)  # deserialization
 
-        data = json.dumps(member_details)
-        Path("member_details.json").write_text(data)
+        member_details = {'name': name, 'age': age, 'gender': gender, 'contact_no': contact_no, 'aadhar_no': aadhar_no,
+                          'package_selection': package, 'amount': amount}
+        existing_data.append(member_details)  # appending value
+        print(existing_data)
+
+        with open("gym/members_data/member_details.json", 'w') as write_file:  # writing the file
+            json.dump(existing_data, write_file)  # serialization
 
 
 if __name__ == '__main__':
     gym = GymMembers()
-    gym.new_member_data("vinothan", 20, 'male', 9789619430,
+    gym.new_member_data("Akash", 20, 'male', 9789619430,
                         111122223333, 'M', 20000)
