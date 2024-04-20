66d559a743ccac512fde3bfe704a4c0e56f7c9ab
Vinothan2003 <vinothannc4@gmail.com>
2024-04-20 11:32:23 +0530

    day 3 new_member : coding finished

--- com/mosh/woking_with_files/work_out_programs/gym/members_data/member_details.py
+++ com/mosh/woking_with_files/work_out_programs/gym/members_data/member_details.py
@@ -1,21 +1,30 @@
 import json
+from com.mosh.woking_with_files.work_out_programs.gym.data_base.gym_details import GymDataBase
 
 
 class GymMembers:
+
     @staticmethod
-    def new_member_data(name, age, gender, contact_no, aadhar_no, package, amount):
-        with open("gym/members_data/member_details.json", 'r') as read_file:  # reding the json file
-            existing_data = json.load(read_file)  # deserialization
+    def new_member_data(name, age, gender, contact_no, aadhar_no, package, amount, valid_date):
+        gym_database = GymDataBase()
 
-        member_details = {'name': name, 'age': age, 'gender': gender, 'contact_no': contact_no, 'aadhar_no': aadhar_no,
-                          'package_selection': package, 'amount': amount}
-        existing_data.append(member_details)  # appending value
+        member_details = [{'name': name, 'age': age, 'gender': gender, 'contact_no': contact_no,
+                           'aadhar_no': aadhar_no,
+                           'package_selection': package, 'amount': amount, 'Validity_date': valid_date}]
 
-        with open("gym/members_data/member_details.json", 'w') as write_file:  # writing the file
-            json.dump(existing_data, write_file)  # serialization
+        with open("gym/members_data/member_details.json", 'w') as write_file:  # reding the json file
+            json.dump(member_details, write_file)  # deserialization
+
+        gym_database.members_data()
+        gym_database.display_members()
+
+        with open("gym/data_base/all_members_details.json", 'r') as read_file:
+            existing_data = json.load(read_file)
+            for member in existing_data:
+                if member['name'] == name:
+                    print(f"Your Gym ID : {member['id']}")
 
 
 if __name__ == '__main__':
     gym = GymMembers()
-    gym.new_member_data("Akash", 20, 'male', 9789619430,
-                        111122223333, 'M', 20000)
+    gym.new_member_data(None, "Akash", "20", 'male', 9789619430, 111122223333, 'M', 20000, 2025 - 9 - 6)
